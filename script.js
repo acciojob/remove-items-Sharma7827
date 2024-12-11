2669c4188ca53887d4641e4a93905ddcf76dc11a
Sharma7827 <sharmas4063@gmail.com>
2024-12-11 20:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let dropdown = document.getElementById("colorSelect")
+let btn = document.getElementById("removeButton")
+
+btn.addEventListener("click", removeElement)
+
+function removeElement() {
+	let selectedIndex = dropdown.selectedIndex
+	if(selectedIndex !== -1){
+		dropdown.options[selectedIndex].remove()
+	}
+	else {
+	    console.log("No option selected");
+	}
+	
+}
